399e5730bcd9bd33c5a9c7d31c2f0e21375631e2
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-03-15 21:59:42 +0200

    Try different responses

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,22 +1,15 @@
-from flask import Flask
-from .package.module import MODULE_VALUE
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return (
-        "Hello!\n"
-        "World!"
-    )
+    return render_template("index.html")
 
-@app.route("/page1")
-def page1():
-    return "Tämä on sivu 1"
-
-@app.route("/page2")
-def page2():
-    return "Tämä on sivu 2"
+@app.route("/beer")
+def beer():
+    words = ["bisse", "kalija", "ölppä"]
+    return render_template("index.html", message="Tervetuloa!", items=words)
 
 @app.route("/test")
 def test():
@@ -32,10 +25,6 @@ def page(id):
 @app.route("/hello/<name>", methods=['GET'])
 def hello(name: str):
     return f"hello {name}"
-
-@app.route("/module")
-def module():
-    return f"loaded from FlaskApp.package.module = {MODULE_VALUE}"
 
 if __name__ == "__main__":
     app.run()
