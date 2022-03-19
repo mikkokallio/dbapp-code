b03c6f8bea01348bb1a6951616bb97f44a673ff3
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-03-20 00:26:38 +0200

    Change env variable name

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -4,7 +4,7 @@ from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["myAppSetting"]
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQL_URI"]
 db = SQLAlchemy(app)
 
 @app.route("/")
