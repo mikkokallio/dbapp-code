ab44e81a9080ce1864f7ea6850964d3f502bd92d
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-03-19 22:52:21 +0200

    Try SQL connection

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,6 +1,36 @@
-from flask import Flask, render_template, request
+from os import environ
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["myAppSetting"]
+db = SQLAlchemy(app)
+
+@app.route("/")
+def list_users():
+    result = db.session.execute("SELECT name FROM users")
+    users = result.fetchall()
+    return render_template("users.html", count=len(users), users=users) 
+
+#@app.route("/new")
+#def new():
+#    return render_template("new.html")
+
+#@app.route("/send", methods=["POST"])
+#def send():
+#    content = request.form["content"]
+#    sql = "INSERT INTO messages (content) VALUES (:content)"
+#    db.session.execute(sql, {"content":content})
+#    db.session.commit()
+#    return redirect("/")
+
+
+
+
+
+
+
 
 
 @app.route("/")
--- tables.sql
+++ tables.sql
@@ -1,21 +1,62 @@
-CREATE TABLE BEER(
-   beer_id SERIAL PRIMARY KEY,
+DROP TABLE users;
+DROP TABLE places;
+DROP TABLE meetups;
+DROP TABLE signups;
+DROP TABLE comments;
+DROP TABLE notifications;
+
+CREATE TYPE GENDER AS ENUM ('male', 'female', 'other');
+CREATE TYPE ROLE as ENUM ('user', 'admin');
+
+CREATE TABLE users (
+   id SERIAL PRIMARY KEY,
    name TEXT NOT NULL,
-   BREWERY_ID        CHAR(50) NOT NULL,
-   ALCOHOL
-   IBU        CHAR(50),
-   SALARY         REAL
+   age INT NOT NULL,
+   gender GENDER NOT NULL,
+   role ROLE NOT NULL,
+   password TEXT NOT NULL,
+   created_at TIMESTAMP
 );
 
-CREATE TABLE [IF NOT EXISTS] table_name (
-   column1 datatype(length) column_contraint,
-   column2 datatype(length) column_contraint,
-   column3 datatype(length) column_contraint,
-   table_constraints
+CREATE TABLE places (
+   id SERIAL PRIMARY KEY,
+   name TEXT NOT NULL,
+   location POINT NOT NULL,
+   created_at TIMESTAMP
 );
 
-CREATE TABLE BREWERY(
-   id INT PRIMARY KEY      NOT NULL,
-   name           CHAR(50) NOT NULL,
-   location        INT      NOT NULL
+CREATE TABLE meetups (
+   id SERIAL PRIMARY KEY,
+   title TEXT NOT NULL,
+   place_id INT REFERENCES places (id),
+   host_id INT REFERENCES places (id),
+   begins_at TIMESTAMP NOT NULL,
+   max_people INT,
+   description TEXT,
+   created_at TIMESTAMP
 );
+
+CREATE TABLE signups (
+   id SERIAL PRIMARY KEY,
+   meetup_id INT REFERENCES meetups (id),
+   user_id INT REFERENCES users (id),
+   created_at TIMESTAMP
+);
+
+CREATE TABLE comments (
+   id SERIAL PRIMARY KEY,
+   meetup_id INT REFERENCES meetups (id),
+   user_id INT REFERENCES users (id),
+   comment TEXT NOT NULL,
+   created_at TIMESTAMP
+);
+
+CREATE TABLE notifications (
+   id serial PRIMARY KEY,
+   user_id INT,
+   message TEXT NOT NULL,
+   created_at TIMESTAMP
+);
+
+
+INSERT INTO users (name, age, gender, role, password, created_at) VALUES ('Jorma', 25, 'male', 'admin', 'password123', NOW());
