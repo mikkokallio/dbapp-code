2127bff59a8bc76bf0ffaee349e88306a9ce4a75
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-04-03 11:54:35 +0300

    Add user info to events

--- FlaskApp/actions.py
+++ FlaskApp/actions.py
@@ -45,11 +45,12 @@ def update_user(username, date_of_birth, gender, description):
 
 
 def upsert_event(username, fields):
-    sql = "INSERT INTO events (title, date, time, description, created_at) VALUES (:title, :date, :time, :description, NOW());"
-
+    sql = "INSERT INTO events (title, host_id, date, time, description, created_at) VALUES (:title, :host_id, :date, :time, :description, NOW());"
+    id = get_user_by_name(username).id
     try:
         db.session.execute(sql, {
             "title": fields["title"],
+            "host_id": id,
             "date": f"{fields['year']}-{fields['month']}-{fields['day']}",
             "time": f"{fields['hours']}:{fields['minutes']}",
             "description": fields["description"]})
@@ -61,7 +62,7 @@ def upsert_event(username, fields):
 
 
 def get_all_events():
-    result = db.session.execute("SELECT * FROM events")
+    result = db.session.execute("SELECT * FROM events LEFT JOIN users ON events.host_id = users.id")
     return result.fetchall()
 
 
